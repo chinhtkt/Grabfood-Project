f2fdb9a8ee019823f6a6d9e84216f43eb3c27503
chinhtkt <47823938+chinhtkt@users.noreply.github.com>
2021-07-05 10:21:36 +0700

    updated

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,17 +1,24 @@
 var quickviews = bulmaQuickview.attach();
-    
-          bulmaCarousel.attach('#carousel-demo', {
-              slidesToScroll: 1,
-              slidesToShow: 4,
-              infinite: true,
-            });
 
-        const navbar = document.querySelector('.navbar');
-        window.onscroll = (e) => {
-          if (window.scrollY > 1) {
-              navbar.classList.add('nav-active');
-              
-          } else { 
-              navbar.classList.remove('nav-active');
-          }
-        };
+bulmaCarousel.attach('#carousel-top', {
+  slidesToScroll: 1,
+  slidesToShow: 5,
+});
+
+bulmaCarousel.attach('#carousel-main', {
+  slidesToScroll: 1,
+  slidesToShow: 4,
+});
+
+
+
+
+
+const navbar = document.querySelector('.navbar');
+window.onscroll = (e) => {
+  if (window.scrollY > 1) {
+    navbar.classList.add('nav-active');
+  } else {
+    navbar.classList.remove('nav-active');
+  }
+};
